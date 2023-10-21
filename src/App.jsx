96dedcbf80938ebe5abeeee99cf6ad35f453c70c
nigam <nigamsharma23112001@gmail.com>
2023-10-21 21:03:37 +0530

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import  authService from './appwrite/auth'
+import {login ,logout} from './store/authSlice'
+import { Footer, Header } from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const dispatch=useDispatch()
+useEffect(() => {
+  authService.getCurrentUser()
+  .then((userData)=>{
+    if (userData){
+dispatch(login({userData}))
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .catch((error)=>{console.log(error)})
+  .finally(()=>setLoading(false))
+}, [])
 
-  return (
+  return !loading?(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen flex flex-wrap bg-gray-900">
+      <div className="w-full block">
+
+    <Header/>
+    <main>
+    login
+    </main>
+    <Footer/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
-  )
+  ):null //assignment
 }
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Logo from "./Logo";
+import Input from "./Input";
+import Container from "./container/Container";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import LogoutButton from "./header/LogoutButton";
+
+export{
+    Footer,
+   Header,
+   Container,
+   Logo,
+   LogoutButton,
+   Input,
+  
+}
